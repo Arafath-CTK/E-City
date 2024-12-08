c2505233d71889d30d637559ccd12fe2e6830dfa
Arafath <arafathctk@gmail.com>
2024-12-08 22:45:37 +0530

    basic setup completed

--- Backend/app.js
+++ Backend/app.js
@@ -1,16 +1,15 @@
+require("dotenv").config();
 const express = require("express");
-const path = require("path");
 const adminRouter = require("./routes/admin");
-const userRouter = require("./routes/user");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 
 const app = express();
 
-app.use(cookieParser()); // For JWT
+app.use(cookieParser());
 app.use(
   session({
-    secret: "your_secret_key", // Replace with a strong secret key
+    secret: process.env.SESSION_SECRET, // Replace with a strong secret key
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }, // Set to true for https only
@@ -20,10 +19,29 @@ app.use(express.urlencoded({ extended: true })); //same use of body parser. its 
 app.use(express.json()); // for parsing json to js object.
 
 app.use("/admin", adminRouter);
-app.use("/", userRouter);
 
 //Server hosting locally
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on : http://localhost:${port}`);
 });
+
+// require('dotenv').config(); // Load environment variables
+// const express = require('express');
+// const mongoose = require('./config/database'); // MongoDB connection
+// const app = express();
+
+// // Middleware
+// app.use(express.json()); // To parse JSON bodies
+
+// // Connect to MongoDB
+// mongoose();
+
+// // Basic route for testing
+// app.get('/', (req, res) => {
+//   res.send('Server is running and MongoDB is connected!');
+// });
+
+// // Start the server
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- Backend/models/admin.js
+++ Backend/models/admin.js
@@ -1,44 +1,8 @@
 const mongoose = require("../config/database");
 
-const adminSchema = new mongoose.Schema(
-  {
-    name: { type: String },
-    email: { type: String, unique: true, lowercase: true },
-    password: { type: String },
-    register_date: { type: String },
-    banner: {
-      mainBanner: {
-        type: Array,
-        required: true,
-      },
-      categoryBanner: [
-        {
-          image: {
-            type: String,
-          },
-        },
-        {
-          image: {
-            type: String,
-          },
-        },
-      ],
-    },
-    coupons: [
-      {
-        code: { type: String, unique: true, uppercase: true },
-        coupon_type: String,
-        discount: Number,
-        start_date: Date,
-        end_date: Date,
-        isActive: Boolean,
-        max_usage: Number,
-        min_cart_value: Number,
-      },
-    ],
-  },
-  { versionKey: false }
-);
-
-const admin = mongoose.model("admin", adminSchema, "admin");
-module.exports = admin;
+const adminSchema = new mongoose.Schema({
+  name: { type: String },
+  email: { type: String, unique: true, lowercase: true },
+  password: { type: String },
+  register_date: { type: String },
+});
