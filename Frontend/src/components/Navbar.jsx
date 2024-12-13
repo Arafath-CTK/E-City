e7a2e3a068f51943fe637ec04c33b2331b607de4
Abhinav-bM <abhinavbmanoj@gmail.com>
2024-12-14 00:13:47 +0530

    basic-redux-setup-and-Footer

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -13,56 +13,53 @@ const Navbar = () => {
   };
 
   return (
-    <header>
-      <nav className="nav-header py-2 mobile:mx-4 lg:mx-20 lg:py-4 relative">
-        <div className="w-full flex items-center lg:py-2">
-          <div className=" md:hidden">
-            <button
-              onClick={handleToggleNavbar}
-              className="flex-1 nav-toggler p-0 border-none mr-5"
-              type="button"
-            >
-              {openNavbar ? (
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="text-white"
-                >
-                  <path
-                    d="M6 18L18 6M6 6L18 18"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="text-white"
-                >
-                  <path
-                    d="M3 6.00098H21M3 12.001H21M3 18.001H21"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              )}
-            </button>
-          </div>
+    <header className="w-full">
+      <nav className="nav-header px-4 border-b w-full flex justify-between items-center py-5">
+        <div className="flex justify-center items-center md:hidden">
+          <button
+            onClick={handleToggleNavbar}
+            className="flex-1 nav-toggler p-0 border-none mr-5"
+            type="button"
+          >
+            {openNavbar ? (
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M6 18L18 6M6 6L18 18"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            ) : (
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3 6.00098H21M3 12.001H21M3 18.001H21"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            )}
+          </button>
+        </div>
 
-          <div className=" mobile:w-full mobile:flex mobile:justify-center lg:flex-1 lg:justify-start">
-            <Link className="nav-brand m-0" href="/home">
-              {/* <div className="position-relative h-full overflow-hidden">
+        <div className="flex justify-center items-center md:justify-start">
+          <Link className="nav-brand m-0" href="/home">
+            {/* <div className="position-relative h-full overflow-hidden">
                     <img
                       src="#"
                       alt="logo"
@@ -72,55 +69,54 @@ const Navbar = () => {
                     />
                   </div> */}
 
-              <span className="xl:text-2xl">StoreName </span>
-            </Link>
-          </div>
+            <span className="xl:text-xl">StoreName </span>
+          </Link>
+        </div>
 
-          <div className="hidden lg:flex w-1/2">
-            <input
-              // ref={searchInputRef}
-              className="w-full border border-gray-300 bg-black rounded-md px-2 py-3 focus:outline-none"
-              type="search"
-              placeholder="Search for products..."
-              aria-label="Search"
-              // onKeyUp={_handleeOnSearchKeyDown}
-            />
-          </div>
+        <div className="hidden md:flex">
+          <input
+            // ref={searchInputRef}
+            className="  w-96 border border-gray-300 rounded-md px-2 py-3 focus:outline-none"
+            type="search"
+            placeholder="Search for products..."
+            aria-label="Search"
+            // onKeyUp={_handleeOnSearchKeyDown}
+          />
+        </div>
 
-          <div className="flex-1 justify-end items-center">
-            <ul className="m-0 p-0 flex justify-end items-center gap-7">
-              <li className="hidden lg:block nav-item dropdown">
-                <Link
-                  // href={loggedIn ? urls.wishlist : urls.login}
-                  className="nav-link dropdown-btn m-0"
-                >
-                  <HeartIcon width={24} height={24} color="white" />
-                </Link>
-              </li>
-              
-              <li className="hidden lg:block nav-item dropdown ">
-                <Link
-                  // href={loggedIn ? urls.profile : urls.login}
-                  className="nav-link dropdown-btn m-0"
-                >
-                  <UserIcon width={24} height={24} color="white" />
-                </Link>
-              </li>
+        <div className="flex items-center">
+          <ul className="m-0 p-0 flex justify-end items-center gap-7">
+            <li className="hidden md:block">
+              <Link
+                // href={loggedIn ? urls.wishlist : urls.login}
+                className="nav-link dropdown-btn m-0"
+              >
+                <HeartIcon width={24} height={24} />
+              </Link>
+            </li>
 
-              <li className="nav-item dropdown col-md-auto mr-1">
-                <Link
-                  // href={loggedIn ? urls.cart : urls.login}
-                  className="relative nav-link nav-with-text m-0"
-                >
-                  <CartIcon width={24} height={24} color="white" />
+            <li className="">
+              <Link
+                // href={loggedIn ? urls.profile : urls.login}
+                className="nav-link dropdown-btn m-0"
+              >
+                <UserIcon width={24} height={24} />
+              </Link>
+            </li>
 
-                  <span className="absolute -top-4 -right-3 bottom-3 bg-blue-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
-                    1
-                  </span>
-                </Link>
-              </li>
-            </ul>
-          </div>
+            <li className="nav-item dropdown col-md-auto mr-1">
+              <Link
+                // href={loggedIn ? urls.cart : urls.login}
+                className="relative nav-link nav-with-text m-0"
+              >
+                <CartIcon width={24} height={24} />
+
+                <span className="absolute -top-4 -right-3 bottom-3 bg-blue-600 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
+                  1
+                </span>
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
 
@@ -155,12 +151,13 @@ const Navbar = () => {
         </div>
       )}
 
-      <div className="hidden md:flex header-menu py-3 justify-center">
-        <ul className="relative m-0 py-2 flex gap-x-6">
+      <div className="hidden md:flex header-menu py-2 justify-center border-y bg-blue-600">
+        {/* HERE GOES THE NAV ELEMENTS WITH DROPDOWN FDF */}
+        <ul className="relative m-0 py-2 flex gap-x-6 text-white">
           <li>Elem1</li>
           <li>Elem2</li>
           <li>Elem3</li>
-          <li>Elwe4</li>
+          <li>Elem4</li>
           <li>Elem5</li>
           {/* {navBar && generateNavElement(navBar.navElements, categories)} */}
         </ul>
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,3 +1,9 @@
+// THINGS TO ADD 
+// 1 : A banner with multiple imaeg - Use swiper slider libray 
+// 2 : A slider , where we list categories or Main products - use swiper slider
+// 3 : Featured product slider - use swiper slider
+
+
 const Home = () => {
   return (
     <section className="min-h-screen flex justify-center items-center">
